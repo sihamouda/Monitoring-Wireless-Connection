0f439f7f48c6ca3cd6a39f8d405945867f4157af
sihamouda <anis.c.hamouda@gmail.com>
2022-10-07 18:21:32 +0100

    show signal every 1s

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import subprocess
 #import re
-#import time
+import time
 #import platform
 #import matplotlib.pyplot as plt
 #import numpy as np
 
+def get_Signal_Power(data):
+    i=int(data.find("Signal level="))
+    return i
+
 def read_data_from_cmd():
     p= subprocess.Popen("iwconfig",stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     out = p.stdout.read().decode()
-    print(out)
+    return(out)
     
+def show_Signal_Power(data):
+    i=0
+    while(i<5):
+        print(data[get_Signal_Power(data)+13:get_Signal_Power(data)+16])
+        i+=1
+        time.sleep(1)
 
-read_data_from_cmd()
+data=read_data_from_cmd()
+show_Signal_Power(data)
