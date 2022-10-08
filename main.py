06cfbf3d2b3dc4beb8b564a6baf45896b14fc06f
sihamouda <anis.c.hamouda@gmail.com>
2022-10-08 11:38:19 +0100

    show dynamic graph of current Signal Power

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import subprocess
 #import re
 import time
+import datetime
 #import platform
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 #import numpy as np
 
 def get_Signal_Power(data):
@@ -13,13 +15,19 @@ def read_data_from_cmd():
     p= subprocess.Popen("iwconfig",stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     out = p.stdout.read().decode()
     return(out)
-    
-def show_Signal_Power(data):
-    i=0
-    while(i<5):
-        print(data[get_Signal_Power(data)+13:get_Signal_Power(data)+16])
-        i+=1
-        time.sleep(1)
 
+curtime,sp=[], []
 data=read_data_from_cmd()
-show_Signal_Power(data)
+
+def show_Signal_Power(i):    
+    curtime.append(datetime.datetime.now())
+    sp.append(data[get_Signal_Power(data)+13:get_Signal_Power(data)+16])
+
+    plt.cla()
+    plt.plot(curtime,sp)
+
+
+
+animate = FuncAnimation(plt.gcf(),show_Signal_Power,interval=1000)
+plt.tight_layout()
+plt.show()
