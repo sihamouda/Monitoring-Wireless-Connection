be4fe51b79b0cb8f8b7b0f9b1525d0ad8308e128
sihamouda <anis.c.hamouda@gmail.com>
2022-11-11 20:04:39 +0100

    socket.io works using threads

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_socketio import SocketIO, emit
 
-import time
+from threading import Lock
 
 import sys
 
@@ -11,9 +11,24 @@ from linux_autoconnect import get_networks, connect_network, detect_best_network
 import linux_common_tools as tools
 from linux_wifi_stats import wifi_statistics
 
+
 # Creating a flask app and using it to instantiate a socket object
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+thread = None
+thread_lock = Lock()
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    while True:
+        socketio.sleep(3)
+        
+        cmd = "nmcli device wifi"
+        networks = get_networks(tools.read_data_from_shell(cmd)[0])
+        
+        socketio.emit("connection",networks)
 
 
 # Handler for default flask route
@@ -26,10 +41,10 @@ def index():
 # Handler for a message recieved over 'connect' channel
 @socketio.on("connect")
 def test_connect():
-    cmd = "nmcli device wifi"
-
-    networks = get_networks(tools.read_data_from_shell(cmd)[0])
-    emit("connection", networks)
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
 
 
 # Notice how socketio.run takes care of app instantiation as well.
