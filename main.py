55489abfbf1a8db28e6f96083aeb851e70ff3a34
sihamouda <anis.c.hamouda@gmail.com>
2022-11-11 12:10:34 +0100

    interface using socket

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO,send, emit
+from flask_socketio import SocketIO, emit
 
+import time
 
 from linux_autoconnect import get_networks, connect_network, detect_best_network
 import linux_common_tools as tools
@@ -17,41 +18,13 @@ socketio = SocketIO(app)
 def index():
     return render_template('front.html')
 
-
 # Handler for a message recieved over 'connect' channel
 @socketio.on('connect')
 def test_connect():
-    emit('connection',  {'status':'connected'})
-
-
-# @socketio.on('json')
-# def handle_json(json):
-#     send(json, json=True)
-
-
-
+    cmd = "nmcli device wifi"
+    networks = get_networks(tools.read_data_from_shell(cmd)[0])
+    emit('connection',  networks)
 
 # Notice how socketio.run takes care of app instantiation as well.
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0')
-
-
-# @app.route("/")
-# def network():
-#     return render_template("front.html")
-
-# @app.route("/socket")
-# def socket():
-#     async def handler(websocket):
-#         while True:
-#             cmd = "nmcli device wifi"
-#             networks = get_networks(tools.read_data_from_shell(cmd)[0])
-#             await websocket.send(json.dumps(networks))
-#             await asyncio.sleep(random.random() * 2 + 1)
-#             # print(f"{networks}")
-
-#     async def main():
-#         async with websockets.serve(handler,"localhost",8080):
-#             await asyncio.Future()
-
-#     asyncio.run(main())
